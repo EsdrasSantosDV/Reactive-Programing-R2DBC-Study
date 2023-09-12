4793e651f3919ac4064efe4a32a45a910a0174a3
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-09-12 19:20:48 -0300

    feat:initial repo

---
+++ src/main/java/com/example/r2dbc/webfluxr2dbcprojectkhan/WebFluxR2dbcProjectKhanApplication.java
@@ -0,0 +1,13 @@
+package com.example.r2dbc.webfluxr2dbcprojectkhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxR2dbcProjectKhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxR2dbcProjectKhanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/r2dbc/webfluxr2dbcprojectkhan/WebFluxR2dbcProjectKhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r2dbc.webfluxr2dbcprojectkhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxR2dbcProjectKhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
